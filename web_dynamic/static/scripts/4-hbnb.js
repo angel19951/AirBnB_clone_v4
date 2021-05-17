d883ba63c7afaa8587b3a9ed0aedacda1d216bbd
Angel Gonzalez <2384@holbertonschool.com>
2021-05-17 02:47:24 +0000

    Adding search bar to filter through amenities for our AirBnb clone

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,78 @@
+const checkedAmenities = {};
+$(document).ready(function () {
+  const checkboxes = $('input');
+  const initialText = $('DIV.amenities h4').text();
+  for (const box of checkboxes) {
+    box.addEventListener('change', function () {
+      if (box.checked) {
+        checkedAmenities[$(box).data('id')] = $(box).data('name');
+      } else {
+        delete checkedAmenities[$(box).data('id')];
+      }
+      const checkedList = Object.values(checkedAmenities);
+      if (checkedList.length < 1) {
+        checkedList.push(initialText);
+      }
+      $('DIV.amenities h4').text(checkedList.join(', '));
+    });
+  }
+  $('button').click(function () {
+    const amenitiesIds = [];
+    for (const id in checkedAmenities) {
+      amenitiesIds.push(id);
+    }
+    const filterDict = { 'amenities': amenitiesIds };
+    $('section.places').empty();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      data: JSON.stringify(filterDict),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: displayPlaces
+    });
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('avalable');
+  }
+});
+
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  data: '{}',
+  dataType: 'json',
+  contentType: 'application/json',
+  success: displayPlaces
+});
+
+function displayPlaces (data) {
+  console.log(data);
+  for (const place of data) {
+    const holder = '<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '') + '</div><div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div></div><div class="description">' + place.description + '</div></article>';
+    $(holder).appendTo('SECTION.places');
+  }
+}
+
+/*$('button').click(function () {
+console.log('ITS PRESSED!!!!')
+    const amenitiesIds = [];
+  for (const id in checkedAmenities) {
+    amenitiesIds.push(id);
+  }
+  const filterDict = { 'amenities': amenitiesIds };
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: JSON.stringyfi(filterDict),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: displayPlaces()
+  });
+});
+*/
